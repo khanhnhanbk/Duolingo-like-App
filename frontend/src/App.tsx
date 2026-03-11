d72c97f6116b58b7b9c1c23583f587b54cedf1cb
Novian Nguyen <novian@Novian.novianpc>
2026-03-11 19:45:08 +0700

    init project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-green-500">Duolingo Clone</h1>
+    </div>
+  );
+}
+
+export default App;
